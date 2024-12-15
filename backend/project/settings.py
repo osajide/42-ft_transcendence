792c50b1923efb6d4ddf48a8f4a962d5bd2aec7a
m-taib <mtaib@WIN-BEJQIN5GU1K.localdomain>
2024-12-15 04:56:55 +0100

    add banned_system, fix middlewawre, update tournament

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -177,7 +177,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=1),  # Set access token expiration time
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=45),  # Set access token expiration time
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),     # Set refresh token expiration time
     'ROTATE_REFRESH_TOKENS': False,                   # Set whether refresh tokens should be rotated
     'BLACKLIST_AFTER_ROTATION': True,                 # Optionally blacklist tokens after rotation
