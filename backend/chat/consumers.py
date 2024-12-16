e28a51246b5def2e32b9636076e80673cf0622e8
OussamaLatitude <osajide5@gmail.com>
2024-12-16 18:05:27 +0100

    block msg flow

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -111,7 +111,7 @@ class	ChatConsumer(AsyncWebsocketConsumer):
 														})
 			elif self.friendship.status == 'blocked':
 				if self.friendship.last_action_by == self.user.id:
-					await self.send(text_data=json.dumps({"alert": f"you blocked {self.user}"}))
+					await self.send(text_data=json.dumps({"alert": f"you have blocked {self.user}"}))
 				else:
 					await self.send(text_data=json.dumps({"alert": f"you've been blocked by {self.friend}"}))
 
