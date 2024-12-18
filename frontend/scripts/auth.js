ade6a797f7aa1d0705108a864034869201ab7ae9
Mohamed Taib <mtaib@c2r4p1.1337.ma>
2024-12-18 23:48:39 +0100

    quarter final works, update consumer game, modify nginix and docker files

--- frontend/scripts/auth.js
+++ frontend/scripts/auth.js
@@ -1,6 +1,6 @@
 async function auth42() {
   let data = "Error";
-  const response = await fetch("http://127.0.0.1:8000/api/intra/redirect/");
+  const response = await fetch(glob_endp + "/api/intra/redirect/");
   if (response.ok) data = await response.json();
   // window.location.href = data.auth_url
 }
